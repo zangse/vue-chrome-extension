6f60af17df001d03ef25f0a8a465be01aac3b7df
zangse <zangse@live.com>
2025-07-22 17:20:11 +0800

    chore: udpate sass

--- hiBookMarks/popup/App.vue
+++ hiBookMarks/popup/App.vue
@@ -6,16 +6,16 @@
 </template>
 
 <script setup>
-import "../assets/style/reset.scss";
-import "../assets/style/icon.scss";
-import Dashboard from "./components/dashboard/index.vue";
+import '../assets/style/reset.scss'
+import '../assets/style/icon.scss'
+import Dashboard from './components/dashboard/index.vue'
 </script>
 
 <style lang="scss" scoped>
 #app {
   width: 420px;
   height: auto;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Avenir", "Helvetica", "Arial", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- hiBookMarks/src/App.vue
+++ hiBookMarks/src/App.vue
@@ -5,14 +5,14 @@
 </template>
 
 <script setup>
-import "../assets/style/reset.scss";
-import "../assets/style/icon.scss";
-import Dashboard from "./components/pages/dashboard/index.vue";
+import '../assets/style/reset.scss'
+import '../assets/style/icon.scss'
+import Dashboard from './components/pages/dashboard/index.vue'
 </script>
 
 <style lang="scss" scoped>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Avenir", "Helvetica", "Arial", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
